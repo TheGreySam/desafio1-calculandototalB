37e265e05c76b9a3bfbe5764d55691ddb0cb9fb1
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2022-11-07 22:40:06 -0400

    commit 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,15 @@
-export default {
-    name: 'CalculatorView',
-    //data: () => ({
-      //cant: "0"
-    //}),
-    
-    methods: {
-      //Calcular() {
-        //  document.querySelector(".cantidad").parseInt = "0"
-        Calcular()  {    
-          let cantidad = document.querySelector("#cantidad").value;
-          let color = document.querySelector("#color").value;
-          document.querySelector("#totaltotal").innerHTML = 
-              `<div class="card ">
-                  
-                   <ul class="list-group list-group-flush >
-                      <li class="list-group-item">Total= $${cantidad * 40000}</li>
-                      <li class="list-group-item">Cantidad= ${cantidad}</li>
-                      <li class="list-group-item">Color= <span 
-                          style="height: 25px; width: 25px; border-radius: 50%; background-color: ${color}; display: inline-block">  
-                      <span/></li>
-                  </ul>
-         
-          
-          </div>`;
-        }
-      }
-    }
+var calcular = function()  {    
+  let cantidad = parseInt(document.querySelector("#cantidad").value);
+  let color = document.querySelector("#color").value;
+  document.querySelector("#totaltotal").innerHTML = 
+  `<div class="card main-bg">               
+    <ul class="list-group list-group-flush p-4>
+      <li class="list-group-item">Total= $${cantidad * 40000}</li>
+      <li class="list-group-item">Cantidad= ${cantidad}</li>
+      <li class="list-group-item">Color= <span 
+        style="height: 25px; width: 25px; border-radius: 50%; background-color: ${color}; display: inline-block">  
+      <span/></li>
+    </ul>       
+  </div>`;
+  
+};
